e7b9a097fac7008bf805c3958acbd88364b8f3a7
DVHuan <huan.dam@mogi.vn>
2021-10-12 10:47:29 +0700

    Init project

---
+++ SortResourceExt/Helpers/XLMHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace SortResourceExt.Helpers
+{
+    public class XLMHelper
+    {
+        /// <summary>
+        /// Sort element by attribute "name" (not recursively).
+        /// </summary>
+        /// <param name="element"></param>
+        public static void SortXML(XElement element)
+        {
+            // Hàm chỉ sort các node "data", attribute "name" tăng dần.
+            const string sortedNode = "data";
+            const string sortedAttr = "name";
+
+            IEnumerable<XNode> nodesToBePreserved = element.Nodes().Where(P => P.GetType() != typeof(XElement));
+            var dataNodes = element.Elements()
+                .Where(n => n.Name == sortedNode)
+                .OrderBy(x => (string)x.Attribute(sortedAttr));
+
+            var otherNodes = element.Elements().Where(n => n.Name != sortedNode);
+
+            element.ReplaceNodes(nodesToBePreserved.Concat(otherNodes.Concat(dataNodes)));
+        }
+    }
+}
---
+++ SortResourceExt/SortResourceCommand.cs
@@ -0,0 +1,100 @@
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using SortResourceExt.Helpers;
+using System;
+using System.ComponentModel.Design;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using Task = System.Threading.Tasks.Task;
+
+namespace SortResourceExt
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class SortResourceCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("375e429d-4fb9-420b-9ad5-bb17130e256b");
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static SortResourceCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in SortResourceCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            var dte = await package.GetServiceAsync(typeof(DTE)) as DTE2;
+            Assumes.Present(dte);
+
+            var commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as IMenuCommandService;
+            Assumes.Present(commandService);
+
+            // Visual studio status bar.
+            var statusbar = await package.GetServiceAsync(typeof(SVsStatusbar)) as IVsStatusbar;
+
+            var cmdId = new CommandID(PackageGuids.guidSortResourceExtPackageCmdSet, PackageIds.SortResourceCommandId);
+            var cmd = new OleMenuCommand((s, e) => { OnExecute(dte, statusbar); }, cmdId)
+            {
+                Supported = false
+            };
+
+            commandService.AddCommand(cmd);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        private static void OnExecute(DTE2 dte, IVsStatusbar statusbar)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+                ProjectItem item = dte.SelectedItems.Item(1).ProjectItem;
+                var path = item?.FileNames[1] ?? "";
+                var xml = XElement.Load(path);
+                XLMHelper.SortXML(xml);
+
+                // Overwritten file.
+                xml.Save(path);
+
+                if (statusbar != null)
+                {
+                    statusbar.SetText("Sort resources successfully.");
+                }
+            }
+            catch
+            {
+                if (statusbar != null)
+                {
+                    statusbar.SetText("Cannot sort resources!");
+                }
+            }
+        }
+    }
+}
